2b6eaaa800d63622baaaef09a3b5a568d35ca376
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-05-08 23:04:23 -0400

    setup cpu route

--- src/main.go
+++ src/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"github.com/gofiber/fiber"
+	"github.com/juanhuttemann/nitr-api/src/cpu"
+)
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello, World!")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", helloWorld)
+	app.Get("/api/v1/cpu", cpu.GetCPU)
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello, World!")
-	})
-
+	setupRoutes(app)
 	app.Listen(3000)
 }
